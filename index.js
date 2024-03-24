6fa002b83bd0a0dbd320b0e51e6c19e52a90a6d0
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-24 15:20:09 +0530

    Added codes

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        io.emit('msg_rcvd', data);
+    })
+});
+
 app.use('/', express.static(__dirname + '/public')) // this middlewere maps that where is the public folder is located
 
-server.listen(3000, () => {
-    console.log("Server started on port 3000")
+server.listen(3001, () => {
+    console.log("Server started on port 3001")
 })
